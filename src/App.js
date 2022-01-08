b014c790c9979c3fa9dc4d3b6f9c5a1375149914
Pierre-Alexis DECALLONNE <padecallonne@gmail.com>
2022-01-08 22:33:05 +0100

    probleme pour ajouter FA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 import MessageEditor from './components/MessageEditor';
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(FontAwesomeIcon);
+library.add(fas);
+library.add(fab);
+library.add(far);
+
 function App() {
   return (
     <div className="box">
--- src/components/MessageEditor.js
+++ src/components/MessageEditor.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 
+
 class MessageEditor extends Component {
   render() {
     return (
       <Fragment>
         <section className=" h-screen bg-gradient-to-br from-blue-600 to-indigo-600 flex justify-center items-center w-full">
-          <form action="" className="form container flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 p-4 w-2/6 m-auto mx-2">
-            <textarea name="messages" id="messages" className="resize-none" />
-            <div className="info"></div>
-            <button
-              className="mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide :hover:bg-gradient-to-tr hover:from-indigo-600 hover:to-blue-700 hover:text-gray-300"
-              type="submit"
-            >
-              Envoyer
-            </button>
+          <form
+            action=""
+            className="form container flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 p-4 w-2/6 m-auto mx-2 h-2/3 justify-end"
+          >
+            <div className="flex justify-end">
+              <div className="text-space relative w-full mr-4">
+                <textarea
+                  name="messages"
+                  id="messages"
+                  className="resize-none w-full "
+                />
+                <div className="info absolute top-0">140</div>
+              </div>
+              <button
+                className="w-2/6 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide :hover:bg-gradient-to-tr hover:from-indigo-600 hover:to-blue-700 hover:text-gray-300"
+                type="submit"
+              >
+                
+                Envoyer
+              </button>
+            </div>
           </form>
         </section>
       </Fragment>
